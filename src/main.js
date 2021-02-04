048242216772b2fdf5473c9eb5905b30ea43a7e4
Artem-PC <talanov_artem@ukr.net>
2021-02-05 00:59:48 +0200

    start

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+import Vuelidate from 'vuelidate'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter);
+Vue.use(Vuelidate);
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
